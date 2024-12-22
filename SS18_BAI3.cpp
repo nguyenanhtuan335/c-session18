b24a646c74fd3320922e196db38bb343bd82c499
nguyenanhtuan335 <nguyenthoithoi0@gmail.com>
2024-12-22 19:10:13 +0700

    Add files via upload

---
+++ SS18_BAI3.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Khoi tao cau truc sinh vien
+struct Student {
+    char name[50];
+    int age;
+    char phoneNumber[15];
+};
+
+int main() {
+    // Khai bao mang sinh vien co 5 phan tu
+    struct Student students[5];
+
+    // Nhap thong tin cho tung sinh vien
+    for (int i = 0; i < 5; i++) {
+        printf("Nhap thong tin cho sinh vien thu %d:\n", i + 1);
+        printf("Nhap ten sinh vien: ");
+        getchar(); // Xoa ky tu new line con lai tu lan nhap truoc
+        fgets(students[i].name, sizeof(students[i].name), stdin);
+        printf("Nhap tuoi sinh vien: ");
+        scanf("%d", &students[i].age);
+        printf("Nhap so dien thoai sinh vien: ");
+        scanf("%s", students[i].phoneNumber);
+    }
+
+    // In thong tin cac sinh vien
+    printf("\nThong tin cac sinh vien:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Sinh vien thu %d:\n", i + 1);
+        printf("Ten: %s", students[i].name); // fgets tu dong them ky tu newline o cuoi chuoi
+        printf("Tuoi: %d\n", students[i].age);
+        printf("So dien thoai: %s\n", students[i].phoneNumber);
+    }
+
+    return 0;
+}
+
